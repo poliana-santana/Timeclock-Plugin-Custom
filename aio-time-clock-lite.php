5ad0f8cf86b715fd1b8139920f9a4669455a8aa6
Desiree Bruce <desireebruce@freeforcharity.org>
2018-10-19 15:13:55 -0700

    added end date to shift object

--- aio-time-clock-lite.php
+++ aio-time-clock-lite.php
@@ -206,9 +206,14 @@ function aio_time_clock_js()
             }
         }
         add_post_meta($new_post_id, 'employee_clock_in_time', $date, true);
+        $day = date("Y/m/d");
+        add_post_meta($new_post_id, 'start_date', $day, true);
+        add_post_meta($new_post_id, 'number_of_volunteers', 1, true);
+        add_post_meta($open_shift_id, 'total_time', 0, true);
         if ($department != null) {
             add_post_meta($new_post_id, 'department', $department, true);
         }
+
         add_post_meta($new_post_id, 'ip_address_in', $_SERVER['REMOTE_ADDR'], true);
         $open_shift_id = $new_post_id;
         $is_clocked_in = true;
@@ -252,9 +257,10 @@ function aio_time_clock_js()
         add_post_meta($open_shift_id, 'employee_clock_out_time', $date, true);
         add_post_meta($open_shift_id, 'ip_address_out', $_SERVER['REMOTE_ADDR'], true);              
         $time_total = aio_date_difference_lite($employee_clock_out_time, $employee_clock_in_time);
-
+        $day = date("Y/m/d");
+        add_post_meta($new_post_id, 'end_date', $day, true);
         $hours_total = aio_date_to_hours($time_total);
-        add_post_meta($open_shift_id, 'total_time', $hours_total, true);
+        update_post_meta($open_shift_id, 'total_time', $hours_total);
 
         echo json_encode(
             array(
